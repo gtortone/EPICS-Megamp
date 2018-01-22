7506db6543303ecc67647a4ae67c9b8eccd1a86c
Gennaro Tortone <gennaro.tortone@na.infn.it>
2018-01-22 11:50:52 +0100

    serial arguments added

--- spikes/ma-bulkwrite.py
+++ spikes/ma-bulkwrite.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
 
+from optparse import OptionParser
+
 import sys
 sys.path.append('../libs')
-
 import megamp
 
-MA = megamp.Megamp("/dev/ttyUSB0", 115200, 0.5)
+port = "/dev/ttyUSB0"
+speed = 115200
+
+parser = OptionParser()
+parser.add_option("-p", action="store", type="string", dest="port", help="specify Megamp serial port (e.g. /dev/ttyUSB0)")
+parser.add_option("-s", action="store", type="string", dest="speed", help="set Megamp serial port speed (e.g. 9600)")
+(options, args) = parser.parse_args()
+
+if(options.port):
+  port = options.port
+
+if(options.speed):
+  speed = options.speed
+
+print("Megamp serial parameter: port = " + port + " speed = " + str(speed))
+
+MA = megamp.Megamp(port, speed, 0.5)
 
 for i in range(0,5):
     try:
